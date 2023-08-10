4d6a336892c76a66628bdc69fe6b07dfd0a4677d
RostikKalabishka <rostik31052002@gmail.com>
2023-08-10 17:25:23 +0300

    add changes

--- lib/features/crypto_list/widgets/crypto_coin_tile.dart
+++ lib/features/crypto_list/widgets/crypto_coin_tile.dart
@@ -1,6 +1,5 @@
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class CryptoCoinTile extends StatelessWidget {
   const CryptoCoinTile({super.key, required this.coin});
@@ -18,8 +17,7 @@ class CryptoCoinTile extends StatelessWidget {
       trailing: const Icon(
         Icons.arrow_forward,
       ),
-      title:
-          Text('${coin.name}', style: Theme.of(context).textTheme.bodyMedium),
+      title: Text(coin.name, style: Theme.of(context).textTheme.bodyMedium),
       subtitle: Text('${coin.priceInUSD} \$ ',
           style: Theme.of(context).textTheme.labelSmall),
       onTap: () {
