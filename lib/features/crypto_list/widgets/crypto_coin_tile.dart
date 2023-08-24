9d2f594b1a0011fcc31c861c21b335b69aeac6a2
RostikKalabishka <rostik31052002@gmail.com>
2023-08-24 19:29:32 +0300

    fix bug

--- lib/features/crypto_list/widgets/crypto_coin_tile.dart
+++ lib/features/crypto_list/widgets/crypto_coin_tile.dart
@@ -1,6 +1,5 @@
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class CryptoCoinTile extends StatelessWidget {
   const CryptoCoinTile({
